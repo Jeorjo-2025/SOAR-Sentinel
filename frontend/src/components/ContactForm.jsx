986f7e325e540333dcbb19f7a04f36531734c8bb
Jeorjo-2025 <YOUR_EMAIL_FOR_JEORJO_2025>
2026-05-22 18:47:56 -0400

    Fix ContactForm export

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -1,29 +1,36 @@
-<form
-  name="contact"
-  method="POST"
-  data-netlify="true"
-  data-netlify-honeypot="bot-field"
->
-  <input type="hidden" name="form-name" value="contact" />
+// src/components/ContactForm.jsx
+function ContactForm() {
+  return (
+    <form
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="form-name" value="contact" />
 
-  <div hidden>
-    <input name="bot-field" />
-  </div>
+      <div hidden>
+        <input name="bot-field" />
+      </div>
 
-  <label>
-    Your Name:
-    <input type="text" name="name" required />
-  </label>
+      <label>
+        Your Name:
+        <input type="text" name="name" required />
+      </label>
 
-  <label>
-    Your Email:
-    <input type="email" name="email" required />
-  </label>
+      <label>
+        Your Email:
+        <input type="email" name="email" required />
+      </label>
 
-  <label>
-    Message:
-    <textarea name="message" required></textarea>
-  </label>
+      <label>
+        Message:
+        <textarea name="message" required></textarea>
+      </label>
 
-  <button type="submit">Send Message</button>
-</form>;
+      <button type="submit">Send Message</button>
+    </form>
+  );
+}
+
+export default ContactForm;
